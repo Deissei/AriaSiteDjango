21ffe92c109ec278d025808f49ca0df499c2e359
Deissei <privetizkyrgyzstana2@gmail.com>
2023-03-21 19:31:16 +0600

    create view, settings view

--- aria/apps/homepage/views.py
+++ aria/apps/homepage/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from .models import SettingMain
 
 class HomePageView(View):
     def get(self, request):
+        # setting
+        settings = SettingMain.objects.get(activate=True)
+
         return render(request, 'home/index.html', locals())
